be14ff5579633fa24caa365ab4eb66c583ab41a6
Suraj Mishra <connect@surajmishra.dev>
2024-07-15 17:03:41 +0530

    Add dummy table data

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,75 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
+async function getData() {
+  const res = await fetch("localhost:3001/api/data");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+function createData(code: string, rate: number, volume: number, cap: number) {
+  return { code, rate, volume, cap };
+}
+
+async function extractData() {
+  const data = await getData();
+  let rows;
+  data.forEach((item: any) => {
+    rows = [createData("Code", item.rate, item.volume, item.cap)];
+  });
+  return rows;
+}
+
+async function data() {
+  let rows = await extractData();
+  console.log("ROWS ==> ", rows);
+}
+
+data();
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <></>
+    // <TableContainer component={Paper}>
+    //   <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    //     <TableHead>
+    //       <TableRow>
+    //         <TableCell>Dessert (100g serving)</TableCell>
+    //         <TableCell align="right">Calories</TableCell>
+    //         <TableCell align="right">Fat&nbsp;(g)</TableCell>
+    //         <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+    //         <TableCell align="right">Protein&nbsp;(g)</TableCell>
+    //       </TableRow>
+    //     </TableHead>
+    //     <TableBody>
+    //       {rows.map((row) => (
+    //         <TableRow
+    //           key={row.name}
+    //           sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+    //         >
+    //           <TableCell component="th" scope="row">
+    //             {row.name}
+    //           </TableCell>
+    //           <TableCell align="right">{row.calories}</TableCell>
+    //           <TableCell align="right">{row.fat}</TableCell>
+    //           <TableCell align="right">{row.carbs}</TableCell>
+    //           <TableCell align="right">{row.protein}</TableCell>
+    //         </TableRow>
+    //       ))}
+    //     </TableBody>
+    //   </Table>
+    // </TableContainer>
   );
 }
